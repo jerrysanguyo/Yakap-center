5d39afd3d12c6b68f9f6efe889ca94d01f2593fc
Jerry Sanguyo Jr <jsanguyo1624@gmail.com>
2025-05-05 16:18:53 +0800

    added: authentication backend

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use App\Http\Requests\Auth\LoginRequest;
+use App\Services\Auth\LoginService;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Auth\AuthenticationException;
+
+class LoginController extends Controller
+{
+    protected $loginService;
+
+    public function __construct(LoginService $loginService)
+    {
+        $this->loginServices = $loginService;
+    }
+
+    public function index()
+    {
+        return view('');
+    }
+
+    public function authenticate(LoginRequest $request)
+    {
+        $ip = request()->ip();
+        $browser = request()->header('User-Agent');
+
+        try {
+            $user = $this->loginService->authenticate($request->validated());
+    
+            activity()
+                ->performedOn($user)
+                ->causedBy($user)
+                ->log('User'. $user->first_name . $user->last_name . " logged in successfully. ({$ip} - {$browser})");
+    
+            return redirect()
+                ->route($user->getRoleNames()->first() . '.dashboard')
+                ->with('success', 'Logged in successfully!');
+        } catch (AuthenticationException $e) {
+            activity()
+                ->log("Failed login attempt: ({$ip} - {$browser})");
+    
+            return redirect()
+                ->route('login')
+                ->with('failed', 'Invalid login credentials.');
+        }
+    }
+
+    public function logout()
+    {
+        $ip = request()->ip();
+        $browser = request()->header('User-Agent');
+        
+        $user = Auth::user();
+        
+        $this->authServices->logout(); 
+        
+        activity()
+            ->performedOn($user)
+            ->causedBy($user)
+            ->log('User'. $user->first_name . $user->last_name . " logged out successfully. ({$ip} - {$browser})");
+
+        return redirect()
+            ->route('login')
+            ->with('success', 'You have successfully logged out!');
+    }
+}
