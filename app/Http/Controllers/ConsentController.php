f0b4094f55525db991275a5bed11ae003a9a8195
Jerry Sanguyo Jr <jsanguyo1624@gmail.com>
2025-05-14 01:24:37 +0800

    added: consent partial backend

--- app/Http/Controllers/ConsentController.php
+++ app/Http/Controllers/ConsentController.php
@@ -3,63 +3,27 @@
 namespace App\Http\Controllers;
 
 use App\Models\Consent;
-use Illuminate\Http\Request;
+use App\Http\Requests\ConsentRequest;
+use App\Services\ConsentService;
 
 class ConsentController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
+    protected $consentService;
+
+    public function __construct(ConsentService $consentService)
+    {
+        $this->consentService = $consentService;
+    }
+
     public function index()
     {
         //
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
+    public function store(ConsentRequest $request)
     {
-        //
-    }
+        $answer = $this->consentService->answer($request->validated());
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(Consent $consent)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Consent $consent)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, Consent $consent)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Consent $consent)
-    {
-        //
+        return $this->consentService->handleRedirect($answer, 'answered');
     }
 }
