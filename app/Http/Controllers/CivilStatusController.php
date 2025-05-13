d772fc27939c981c474dbf73cc6271b9d61f1c1c
Jerry Sanguyo Jr <jsanguyo1624@gmail.com>
2025-05-13 15:47:42 +0800

    added: civil status cms backend

--- app/Http/Controllers/CivilStatusController.php
+++ app/Http/Controllers/CivilStatusController.php
@@ -3,63 +3,58 @@
 namespace App\Http\Controllers;
 
 use App\Models\CivilStatus;
-use Illuminate\Http\Request;
+use App\Http\Requests\CmsRequest;
+use App\DataTables\CmsDataTable;
+use App\Services\CmsService;
 
 class CivilStatusController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
+    protected CmsService $cmsService;
+    protected string $resource = 'civil';
+    protected string $table = 'civil_statuses';
+
+    public function __construct()
     {
-        //
+        $this->cmsService = new CmsService(CivilStatus::class);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
+    public function index(CmsDataTable $dataTable)
     {
-        //
-    }
+        $page_title = 'Civil status';
+        $resource = $this->resource;
+        $column = ['id', 'name', 'remarks', 'action'];
+        $data = CivilStatus::getAllCivilStatuses();
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
+        return $dataTable
+            ->render('cms.index', compact(
+                'page_title',
+                'resource', 
+                'column',
+                'data',
+                'dataTable'
+            ));
+    }
+    
+    public function store(CmsRequest $request)
     {
-        //
-    }
+        $request->merge(['cms_table' => $this->table]);
+        $store = $this->cmsService->cmsStore($request->validated());
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(CivilStatus $civilStatus)
+        return $this->cmsService->handleRedirect($store, $this->resource, 'created');
+    }
+    
+    public function update(CmsRequest $request, CivilStatus $civilStatus)
     {
-        //
-    }
+        $request->merge(['cms_table' => $this->table, 'id' => $civilStatus->id]);
+        $update = $this->cmsService->cmsUpdate($request->validated, $civilStatus->id);
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(CivilStatus $civilStatus)
-    {
-        //
+        return $this->cmsService->handleRedirect($update, $this->resource, 'updated');
     }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, CivilStatus $civilStatus)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
+    
     public function destroy(CivilStatus $civilStatus)
     {
-        //
+        $destroy = $this->cmsService->cmsDestroy($civilStatus->id);
+
+        return $this->cmsService->handleRedirect($destroy, $this->resource, 'deleted');
     }
 }
